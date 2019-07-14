90c8c066fc5521ae6fbf1db4c61a4fb18b4b3a7e
Quim Granados <quimgranados@gmail.com>
2019-07-14 21:45:52 +1000

    add image to team model

--- src/models/team.js
+++ src/models/team.js
@@ -14,11 +14,27 @@ const teamSchema = new mongoose.Schema({
     players: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Users'
-    }]
+    }],
+    image: {
+        type: Buffer,
+        default: null
+    }
 },
 {
     timestamps: true
 });
+
+// userSchema.methods is for an specific "user" methods
+teamSchema.methods.toJSON = function() {
+    
+    const teamImage = this;
+    const teamImageObject = teamImage.toObject();
+
+    // delete this data to not send it back as a response
+    delete teamImageObject.image;   
+
+    return teamImageObject;
+};
 
 const Team = mongoose.model('Team', teamSchema);
 
