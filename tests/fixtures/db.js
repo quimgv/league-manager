4da91bc4323cdd4db0e780ba63a584d20594d6b7
Quim Granados <quimgranados@gmail.com>
2019-07-15 18:06:08 +1000

    update user tests

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -5,8 +5,9 @@ const User = require('../../src/models/user');
 const userOneID = new mongoose.Types.ObjectId();
 const userOne = {
     _id: userOneID,
-    name: 'Ana Delia',
-    email: 'anadeliaortgor@test.com',
+    firstName: 'Quim',
+    lastName: 'Granados',
+    email: 'quim@test.com',
     password: 'test123',
     tokens: [{
         token: jwt.sign({ _id: userOneID }, process.env.JWT_SECRET)
@@ -16,8 +17,9 @@ const userOne = {
 const userTwoID = new mongoose.Types.ObjectId()
 const userTwo = {
     _id: userTwoID,
-    name: 'Marçal Sydney',
-    email: 'marci@test.com',
+    firstName: 'Ana Delia',
+    lastName: 'Ortega',
+    email: 'ana@test.com',
     password: 'test123',
     tokens: [{
         token: jwt.sign({ _id: userTwoID }, process.env.JWT_SECRET)
--- tests/user.test.js
+++ tests/user.test.js
@@ -9,7 +9,8 @@ test('Should signup a new user', async () => {
     const response = await request(app)
         .post('/users')
         .send({
-            name: 'Quim',
+            firstName: 'Quim',
+            lastName: 'Granados',
             email: 'quimgranados@gmail.com',
             password: 'test123'
         })
@@ -20,7 +21,8 @@ test('Should signup a new user', async () => {
 
     expect(response.body).toMatchObject({
         user: {
-            name: 'Quim',
+            firstName: 'Quim',
+            lastName: 'Granados',
             email: 'quimgranados@gmail.com'
         },
         token: user.tokens[0].token
@@ -54,7 +56,7 @@ test('Should not login unexisting user', async () => {
 test('Should get profile for user', async () => {
     await request(app)
         .get('/users/me')
-        .set('x-auth-token', 'Bearer ' + userOne.tokens[0].token)
+        .set('Authorization', 'Bearer ' + userOne.tokens[0].token)
         .send()
         .expect(200)
 })
@@ -69,7 +71,7 @@ test('Should not get profile for unauthenticated user', async () => {
 test('Should delete account for user', async () => {
     const response = await request(app)
         .delete('/users/me')
-        .set('x-auth-token', 'Bearer ' + userOne.tokens[0].token)
+        .set('Authorization', 'Bearer ' + userOne.tokens[0].token)
         .send()
         .expect(200)
 
@@ -88,7 +90,7 @@ test('Should not delete account for unauthenticated user', async () => {
 test('Should upload avatar', async () => {
     await request(app)
         .post('/users/me/avatar')
-        .set('x-auth-token', 'Bearer ' + userOne.tokens[0].token)
+        .set('Authorization', 'Bearer ' + userOne.tokens[0].token)
         .attach('avatar', 'tests/fixtures/profile-pic.jpg')
         .expect(200)
 
@@ -99,21 +101,21 @@ test('Should upload avatar', async () => {
 test('Should update valid user fields', async () => {
     const response = await request(app)
         .patch('/users/me')
-        .set('x-auth-token', 'Bearer ' + userOne.tokens[0].token)
+        .set('Authorization', 'Bearer ' + userOne.tokens[0].token)
         .send({
-            name: 'Pedro'
+            firstName: 'Pedro'
         })
         expect(200)
 
     const user = await User.findById(response.body._id)
-    expect(user.name).toEqual('Pedro')
+    expect(user.firstName).toEqual('Pedro')
     
 })
 
 test('Should not update invalid user fields', async () => {
     const response = await request(app)
         .patch('/users/me')
-        .set('x-auth-token', 'Bearer ' + userOne.tokens[0].token)
+        .set('Authorization', 'Bearer ' + userOne.tokens[0].token)
         .send({
             location: 'Pedro'
         })
